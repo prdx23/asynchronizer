7841ac4b88b3e77c8bd8a9719e717b80eb6dbf94
arsh23 <programmer.arsh@gmail.com>
2016-07-19 01:26:25 +0530

    upgraded to decorator

--- async.py
+++ async.py
@@ -8,35 +8,39 @@ import time
 
 
 class asynchronizer():
-
     def __init__(self,workers=32):
         self.workers = workers
         self.pool = gevent.pool.Pool(self.workers)
         self.pqueue = gevent.queue.PriorityQueue()
 
-    def add(self,priority,func,params):
-        self.pqueue.put_nowait((priority,func,params))
+    def add(self,priority,func,*args,**kwargs):
+        self.pqueue.put_nowait((priority,func,args,kwargs))
 
     def run(self):
         while not self.pqueue.empty() and not self.pool.full():
             for x in xrange(0, min(self.pqueue.qsize(), self.pool.free_count())):
                 t = self.pqueue.get_nowait()
-                self.pool.start(self.pool.spawn(t[1],t[2]))
+                self.pool.start(self.pool.spawn(t[1],*t[2],**t[3]))
             self.pool.join()
+
+
+def asynchronize(func):
+    def converted_func(*args,**kwargs):
+        a.add(1,func,*args,**kwargs)
+    return converted_func
 
 
 a = asynchronizer(5)
 
-
+@asynchronize
 def func(i):
     print 'loop',i
     if i*100 < 1000:
-        a.add(0,func,i*100)
+        func(i*100)
     time.sleep(1)
 
 
 for i in range(1,10):
-    a.add(1,func,i)
-
+    func(i)
 
 a.run()
