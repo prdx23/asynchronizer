062e0479cbbd8d2662101455845bb564fbd5f304
arsh23 <programmer.arsh@gmail.com>
2016-07-18 17:41:12 +0530

    priority queue implemented

--- async.py
+++ async.py
@@ -9,23 +9,23 @@ import requests
 
 max_workers = 5
 pool = gevent.pool.Pool(max_workers)
-queue = gevent.queue.Queue()
+queue = gevent.queue.PriorityQueue()
 
 
 def func(i):
-
     print 'loop',i
-    queue.put_nowait((func,i*100))
+    if i*100 < 1000:
+        queue.put_nowait((0,func,i*100))
     time.sleep(1)
 
 
-for i in range(10):
-    queue.put_nowait((func,i))
+for i in range(1,10):
+    queue.put_nowait((1,func,i))
 
 
 
 while not queue.empty() and not pool.full():
     for x in xrange(0, min(queue.qsize(), pool.free_count())):
         t = queue.get_nowait()
-        pool.start(pool.spawn(t[0],t[1]))
+        pool.start(pool.spawn(t[1],t[2]))
     pool.join()
