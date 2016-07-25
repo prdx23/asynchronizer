1211c26641da57c385ca2779448a13a7c35c52c6
arsh23 <programmer.arsh@gmail.com>
2016-07-25 21:06:10 +0530

    major code changes

--- asynchronizer/__init__.py
+++ asynchronizer/__init__.py
@@ -13,21 +13,34 @@ class asynchronizer():
 
     def add(self,priority,func,*args,**kwargs):
         # this function adds other functions to the priority queue
-        self.pqueue.put_nowait((priority,func,args,kwargs))
+        self.pqueue.put((priority,func,args,kwargs))
+        self.startWorkers()
 
     def updateWorkers(self,workers):
-        # can be used to update no. of workers if run() isn't called
+        # can be used to update no. of workers if -
+        # nothing is added to the pool yet
         self.workers = workers
         self.pool = gevent.pool.Pool(self.workers)
 
-    def run(self):
-        # this function starts the gevent pool
-        # this is a blocking function, so should be called last
-        while not self.pqueue.empty() and not self.pool.full():
-            for x in range(0,min(self.pqueue.qsize(),self.pool.free_count())):
+    def worker(self):
+        # this is the worker that runs inside a greenlet thread
+        # this will process the queue till there are no items left
+        while True:
+            try:
                 p,func,args,kwargs = self.pqueue.get_nowait()
-                self.pool.start(self.pool.spawn(func,*args,**kwargs))
-            self.pool.join()
+                func(*args,**kwargs)
+            except gevent.queue.Empty:
+                return
+
+    def startWorkers(self):
+        # this function spawns the required number of greenlet threads
+        for x in range(0,min(self.pqueue.qsize(),self.pool.free_count())):
+            self.pool.spawn(self.worker)
+
+    def wait(self):
+        # this function will wait for all the greenlet threads to finish
+        # this is a blocking function
+        self.pool.join()
 
 a = asynchronizer(32)
 
@@ -43,9 +56,9 @@ def asynchronize(func):
         a.add(priority,func,*args,**kwargs)
     return converted_func
 
-def startPool():
-    # wrapper for asynchronizer.run()
-    a.run()
+def Wait():
+    # wrapper for asynchronizer.wait()
+    a.wait()
 
 def setWorkers(workers):
     # wrapper for asynchronizer.updateWorkers()
