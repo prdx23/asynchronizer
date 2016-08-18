506c88bfb628987a74b305dc0fb957f43cddce3f
arsh23 <programmer.arsh@gmail.com>
2016-08-18 10:09:16 +0530

    added pre commit hooks

--- asynchronizer/__init__.py
+++ asynchronizer/__init__.py
@@ -1,22 +1,24 @@
+# -*- coding: utf-8 -*-
+import gevent.pool
+import gevent.queue
 from gevent import monkey
 monkey.patch_all()
 
-import gevent.pool
-import gevent.queue
 
 class asynchronizer():
-    def __init__(self,workers):
+
+    def __init__(self, workers):
         # workers define how many concurrent functions should be run
         self.workers = workers
         self.pool = gevent.pool.Pool(self.workers)
         self.pqueue = gevent.queue.PriorityQueue()
 
-    def add(self,priority,func,*args,**kwargs):
+    def add(self, priority, func, *args, **kwargs):
         # this function adds other functions to the priority queue
-        self.pqueue.put((priority,func,args,kwargs))
+        self.pqueue.put((priority, func, args, kwargs))
         self.startWorkers()
 
-    def updateWorkers(self,workers):
+    def updateWorkers(self, workers):
         # can be used to update no. of workers if -
         # nothing is added to the pool yet
         self.workers = workers
@@ -27,14 +29,14 @@ class asynchronizer():
         # this will process the queue till there are no items left
         while True:
             try:
-                p,func,args,kwargs = self.pqueue.get_nowait()
-                func(*args,**kwargs)
+                p, func, args, kwargs = self.pqueue.get_nowait()
+                func(*args, **kwargs)
             except gevent.queue.Empty:
                 return
 
     def startWorkers(self):
         # this function spawns the required number of greenlet threads
-        for x in range(0,min(self.pqueue.qsize(),self.pool.free_count())):
+        for x in range(0, min(self.pqueue.qsize(), self.pool.free_count())):
             self.pool.spawn(self.worker)
 
     def wait(self):
@@ -44,21 +46,26 @@ class asynchronizer():
 
 a = asynchronizer(32)
 
+
 def asynchronize(func):
-    def converted_func(*args,**kwargs):
-        priority = kwargs['priority']  if 'priority' in kwargs else 1
+    def converted_func(*args, **kwargs):
+        priority = kwargs['priority'] if 'priority' in kwargs else 1
 
         # remove these two parameters , as they are used in add()
         # temporary fix
-        if 'priority' in kwargs: del kwargs['priority']
-        if 'func' in kwargs: del kwargs['func']
+        if 'priority' in kwargs:
+            del kwargs['priority']
+        if 'func' in kwargs:
+            del kwargs['func']
 
-        a.add(priority,func,*args,**kwargs)
+        a.add(priority, func, *args, **kwargs)
     return converted_func
+
 
 def Wait():
     # wrapper for asynchronizer.wait()
     a.wait()
+
 
 def setWorkers(workers):
     # wrapper for asynchronizer.updateWorkers()
