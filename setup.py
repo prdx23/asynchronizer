d90cfffcdd835ab0a37c3f07c0494cf69696668f
arsh23 <programmer.arsh@gmail.com>
2016-08-18 10:21:32 +0530

    minor changes to add()

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
-
-from setuptools import setup, find_packages
-
+# -*- coding: utf-8 -*-
 from codecs import open
 from os import path
+
+from setuptools import find_packages
+from setuptools import setup
 
 here = path.abspath(path.dirname(__file__))
 
@@ -12,29 +13,29 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='asynchronizer',
-    version='0.2.3',
+    version='0.2.4',
     description='A simple module to make functions asynchronous',
     long_description=long_description,
     url='https://github.com/Arsh23/asynchronizer',
-    download_url = 'https://github.com/Arsh23/asynchronizer/tarball/0.2.3',
+    download_url='https://github.com/Arsh23/asynchronizer/tarball/0.2.4',
     author='arsh23',
     author_email='programmer.arsh@gmail.com',
     license='MIT',
     classifiers=[
-    'Development Status :: 3 - Alpha',
+        'Development Status :: 3 - Alpha',
 
-    'Intended Audience :: Developers',
-    'Topic :: Software Development',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development',
 
-    'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: MIT License',
 
-    'Programming Language :: Python :: 2',
-    'Programming Language :: Python :: 2.6',
-    'Programming Language :: Python :: 2.7',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.2',
-    'Programming Language :: Python :: 3.3',
-    'Programming Language :: Python :: 3.4'
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4'
     ],
 
     keywords='async asynchronous gevent',
