50a12d89676386da8ee39d6555627972dd752716
arsh23 <programmer.arsh@gmail.com>
2016-08-29 14:07:15 +0530

    implemented Actor class

--- examples/temp.py
+++ examples/temp.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import multiprocessing
+import time
 
 
 class Actor(multiprocessing.Process):
@@ -16,11 +17,32 @@ class Actor(multiprocessing.Process):
                 if item is None:
                     break
                 self.recieve(item)
+
             except EOFError:
                 break
-
         self.inbox.close()
         self.pipe.close()
 
     def recieve(self, item):
         raise NotImplementedError
+        time.sleep(0.5)
+        print self.name, 'recieved', item
+        time.sleep(0.5)
+
+
+processes = []
+for i in range(5):
+    p = Actor()
+    processes.append(p)
+
+
+for proc in processes:
+    for i in range(5):
+        proc.inbox.send(i)
+    print 'send'
+
+for proc in processes:
+    proc.inbox.send(None)
+
+for proc in processes:
+    proc.join()
