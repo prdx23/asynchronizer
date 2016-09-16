f3951371bedc145806e77fd90bdb339a0bcb9f1a
arsh23 <programmer.arsh@gmail.com>
2016-09-17 01:28:33 +0530

    rewrote class asynchronizer

--- asynchronizer/asynchronizer.py
+++ asynchronizer/asynchronizer.py
@@ -8,8 +8,11 @@ import gevent.queue
 from gevent import monkey
 monkey.patch_all()
 
-default_w = 4
-default_sp = 4
+DEFAULT_W = 4
+try:
+    DEFAULT_SP = multiprocessing.cpu_count()
+except:
+    DEFAULT_SP = 4
 
 
 class Actor(multiprocessing.Process):
@@ -28,7 +31,7 @@ class Actor(multiprocessing.Process):
             except EOFError:
                 break
             except IOError:
-                # TODO : mention the gevent-mp socket pipe problem
+                # TODO : mention the gevent-mp socket pipe problem in doc
                 continue
 
         self.inbox.close()
@@ -40,10 +43,9 @@ class Actor(multiprocessing.Process):
 
 class Subprocess(Actor):
 
-    def __init__(self, max_workers=default_w, skip_gevent=False):
+    def __init__(self, max_workers=DEFAULT_W):
         Actor.__init__(self)
         self.max_workers = max_workers
-        self.skip_gevent = skip_gevent
         self.pool = gevent.pool.Pool(max_workers)
         self.p_queue = gevent.queue.PriorityQueue()
         self.start()
@@ -58,9 +60,8 @@ class Subprocess(Actor):
                 break
 
     def recieve(self, item):
-
         # print self.name, 'recieved function'
-        if self.skip_gevent is True:
+        if self.max_workers <= 0:
             if item is None:
                 return
             p, pickled_func, args, kwargs = item
@@ -70,7 +71,6 @@ class Subprocess(Actor):
 
         if item is None:
             self.pool.join()
-            return
 
         self.p_queue.put_nowait(item)
         for x in range(0, min(self.p_queue.qsize(), self.pool.free_count())):
@@ -79,82 +79,81 @@ class Subprocess(Actor):
 
 class Asynchronizer():
 
-    def __init__(self,
-                 max_processes=None, skip_mp=False, max_w=None, skip_g=None):
-        if max_processes is None and skip_mp is False:
-            try:
-                self.max_processes = multiprocessing.cpu_count()
-            except:
-                self.max_processes = default_sp
-        elif skip_mp is True:
-            self.max_processes = 1
-        else:
-            self.max_processes = max_processes
+    def __init__(self):
+        self.max_processes = DEFAULT_SP
+        self.max_workers = DEFAULT_W
+        self.counter = 0
         self.processes = []
-        self.p = 0
-        # TODO : fix naming of these
-        self.max_w = max_w if max_w is not None else default_w
-        self.skip_g = skip_g
 
-    def spawn_processes(self, priority, pickled_func, args, kwargs):
+    def set_processes_no(self, n=None, skip_mp=False):
+        if skip_mp is True or n == 0:
+            self.max_processes = 1  # temp , reset to 0
+        elif skip_mp is False:
+            self.max_processes = DEFAULT_SP if (n is None or n < 0) else n
+
+    def set_workers_no(self, n=None, skip_gv=False):
+        if skip_gv is True or n == 0:
+            self.max_workers = 0
+        elif skip_gv is False:
+            self.max_workers = DEFAULT_W if (n is None or n < 0) else n
+
+    def wait(self):
+        for proc in self.processes:
+            proc.join()
+
+    def end(self):
+        for proc in self.processes:
+            proc.inbox.send(None)
+        for proc in self.processes:
+            proc.join()
+
+    def spawn_processes(self, priority, func, args, kwargs):
+        # TODO : error handling
+        pickled_func = dill.dumps(func)
         item = (priority, pickled_func, args, kwargs)
 
         if len(self.processes) < self.max_processes:
-            if self.skip_g is False:
-                s = Subprocess(max_workers=self.max_w, skip_gevent=False)
-            else:
-                s = Subprocess(max_workers=self.max_w, skip_gevent=True)
+            s = Subprocess(max_workers=self.max_workers)
             s.inbox.send(item)
             self.processes.append(s)
             return
 
         # TODO : optimize this, evenly distribute
         # TODO : add check for crashed processes
-        self.p = (self.p + 1) % self.max_processes
-        self.processes[self.p].inbox.send(item)
-
-    def wait_async(self):
-        for proc in self.processes:
-            proc.join()
-
-    def end_async(self):
-        for proc in self.processes:
-            proc.inbox.send(None)
-        for proc in self.processes:
-            proc.join()
+        self.counter = (self.counter + 1) % self.max_processes
+        self.processes[self.counter].inbox.send(item)
 
 
-a = Asynchronizer(skip_mp=False)
-# TODO : func for custom workers/subprocesses
+a = Asynchronizer()
 
 
 def asynchronize(async_func):
     @wraps(async_func)
     def converted_func(*args, **kwargs):
-        # TODO : error handling
-        pickled_func = dill.dumps(async_func)
-        a.spawn_processes(1, pickled_func, args, kwargs)
+        a.spawn_processes(1, async_func, args, kwargs)
     return converted_func
 
 
-def end_async():
-    a.end_async()
+def end():
+    a.end()
 
 
-def config_async(
-        processes=None, greenlets=None, skip_mp=False, skip_gevent=False):
+def wait():
+    a.wait()
 
-    if processes is not None:
-        a.max_processes = processes if processes > 0 else 1
 
-    if greenlets is not None:
-        if greenlets > 0:
-            a.max_w = greenlets
-        else:
-            a.skip_g = True
+def config(
+        processes=None,
+        greenlets=None,
+        skip_mp=False,
+        skip_gevent=False):
 
     if skip_mp is True:
-        a.max_processes = 1
+        a.set_processes_no(skip_mp=True)
+    else:
+        a.set_processes_no(processes)
 
     if skip_gevent is True:
-        a.skip_g = True
+        a.set_processes_no(skip_gv=True)
+    else:
+        a.set_processes_no(processes)
